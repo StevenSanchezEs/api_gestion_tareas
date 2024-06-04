1c50cccb629eecf717ac58a6228c973a4d418bf5
Steve Sanchez <1steven.nathan.sanchez@gmail.com>
2024-06-04 14:32:45 -0600

    Update - Remover importaciones __future__ en gestion_tareas/__init__.py y gestion_tareas/celery.py

--- gestion_tareas/__init__.py
+++ gestion_tareas/__init__.py
@@ -1,7 +1,4 @@
 # gestion_tareas/__init__.py
-
-from __future__ import absolute_import, unicode_literals
-
 # Esto asegura que Celery se cargue cuando Django se inicia.
 from .celery import app as celery_app
 
--- gestion_tareas/celery.py
+++ gestion_tareas/celery.py
@@ -1,6 +1,4 @@
 # gestion_tareas/celery.py
-
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
